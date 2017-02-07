c1941e1615632fcf481669ff679f7c910c2c525e
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-07 01:15:38 -0800

    Completed 115 Input Validation Data-Driven

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -10,10 +10,13 @@ export class DataDrivenComponent {
   myForm: FormGroup;
 
   constructor() {
+    //noinspection TsLint
     this.myForm = new FormGroup({
-      'username': new FormControl(),
-      'email': new FormControl(),
-      'password': new FormControl,
+      'username': new FormControl('Max', Validators.required),
+      'email': new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+      'password': new FormControl('', Validators.required),
     });
   }
 
