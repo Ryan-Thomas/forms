f8de2cbdc1c5177a346294eed5335465bdbca1e7
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-07 01:40:40 -0800

    Completed 118 Form Groups Data-Driven

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-data-driven',
-    templateUrl: 'data-driven.component.html'
+  moduleId: module.id,
+  selector: 'app-data-driven',
+  templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
   myForm: FormGroup;
@@ -12,10 +12,12 @@ export class DataDrivenComponent {
   constructor() {
     //noinspection TsLint
     this.myForm = new FormGroup({
-      'username': new FormControl('Max', Validators.required),
-      'email': new FormControl('', [
-        Validators.required,
-        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+      'userData': new FormGroup({
+        'username': new FormControl('Max', Validators.required),
+        'email': new FormControl('', [
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+      }),
       'password': new FormControl('', Validators.required),
     });
   }
