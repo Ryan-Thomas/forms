ef7ec14b59fc01a60c3b62dd0340f861cd052111
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-09 00:34:42 -0800

    Completed 121 Creating a Form with the FormBuilder Data-Driven

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import {Component, Inject} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormArray, FormBuilder} from '@angular/forms';
 import {validateConfig} from '@angular/router/src/config';
 
 @Component({
@@ -15,21 +15,39 @@ export class DataDrivenComponent {
     'female',
   ];
 
-  constructor() {
+  constructor(@Inject(FormBuilder) fb: FormBuilder) {
     //noinspection TsLint
-    this.myForm = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl('Max', Validators.required),
-        'email': new FormControl('', [
+    this.myForm = fb.group({
+      'userData': fb.group({
+        'username': ['Max', Validators.required],
+        // 'username': new FormControl('Max', Validators.required),
+        'email': ['', [
           Validators.required,
-          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+          )]],
       }),
-      'password': new FormControl('', Validators.required),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([
-        new FormControl('Cooking', Validators.required),
+      'password': ['', Validators.required],
+      'gender': ['male'],
+      'hobbies': fb.array([
+        ['Cooking', Validators.required],
       ]),
     });
+    //
+    // // noinspection TsLint
+    // this.myForm = new FormGroup({
+    //   'userData': new FormGroup({
+    //     'username': new FormControl('Max', Validators.required),
+    //     'email': new FormControl('', [
+    //       Validators.required,
+    //       Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+    //   }),
+    //   'password': new FormControl('', Validators.required),
+    //   'gender': new FormControl('male'),
+    //   'hobbies': new FormArray([
+    //     new FormControl('Cooking', Validators.required),
+    //   ]),
+    // });
+
   }
 
   onAddHobby() {
